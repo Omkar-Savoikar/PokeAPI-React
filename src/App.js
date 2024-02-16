972c020666595122508afda7243d5bf01ee3200d
Omkar-Savoikar <omkarsavoikar@gmail.com>
2024-02-16 12:42:20 +0530

    Add About component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import DexHolder from './components/DexHolder';
+import Pokedex from './components/Pokedex';
+import About from './components/About';
+import PokemonDetails from './components/PokemonDetails';
 
 function App() {
 	return (
-		<div className="container">
-			
+		<div>
+			<Navbar />
+			<Router>
+				<Routes>
+					<Route path="/" exact element={<DexHolder />} />
+					<Route path="/pokedex/:id" element={<Pokedex />} />
+					<Route path="/pokemon/:id" element={<PokemonDetails />} />
+					<Route path="/about" exact element={<About />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+	<div className='container text-white'>
+	  This is about page
+	  Todo
+	</div>
+  )
+}
